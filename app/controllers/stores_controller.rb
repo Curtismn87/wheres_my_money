5a09d6ebe1fa3fe56c4dc4b3192e3bc702222f93
Matt Curtis <Curtismn87@yahoo.com>
2015-08-03 15:25:00 -0400

    Added Edit to stores

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -7,14 +7,28 @@ class StoresController < ApplicationController
     @store = Store.find(params[:id])
   end
 
-def storeAmount
-  @store = Store.find(params[:id])
-  @store.items.each do |items|
-    @store.total_spent = items.amount
-    @store.update()
+  def new
+    @store = Store.new
   end
-end
+  def create
+    @store = Store.create(store_params)
+    redirect_to stores_path
+  end
 
+  def edit
+      @store = Store.find(params[:id])
+  end
 
+  def update
+      @store = Store.find(params[:id])
+      @store.update(store_params)
+      redirect_to store_path(@store)
+  end
+
+  private
+
+  def store_params
+    params.require(:store).permit(:name, :total_spent, :created_at)
+  end
 
 end
