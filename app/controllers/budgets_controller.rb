220949458af3acebbc79f4e17ee8d204a02f4871
Matt Curtis <Curtismn87@yahoo.com>
2015-08-04 17:37:01 -0400

    Added ability to guess stores

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -2,32 +2,31 @@ class BudgetsController < ApplicationController
 
 
   def show
+    @checker = []
+    calculate_total_spent
   end
 
   def check
-    puts budget_params["checker"]
-    @test = budget_params["checker"]
-    puts @test
+    calculate_total_spent
+    @user_input = budget_params["checker"]
     @checker = []
     Store.all.each do |store|
-      @test.each do |testing|
-      if store.name == testing
-        @checker.push(store.name)
+      @user_input.each do |input|
+        if store.name == input
+          @checker.push(store.name)
+        end
       end
-    end
     end
     render :show
   end
-# def check
-#   puts budget_params["checker"]
-#   @checker = []
-#   Store.all.each do |store|
-#     if store.name == budget_params["checker"]
-#       @checker.push(budget_params["checker"])
-#     end
-#   end
-#   render :show
-# end
+
+def calculate_total_spent
+  @total_spent = 0
+  @stores = Store.all
+  @stores.each do |spent|
+    @total_spent = @total_spent + spent.total_spent
+  end
+end
 
 private
 def budget_params
