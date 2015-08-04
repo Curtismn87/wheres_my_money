41294b70afdc82a511d131e585d89409d3155755
Matt Curtis <Curtismn87@yahoo.com>
2015-08-04 11:11:33 -0400

    Added ability to check database for values

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,7 +1,24 @@
 class BudgetsController < ApplicationController
 
+
   def show
-    @stores = Store.all
   end
+
+
+def check
+  puts budget_params["checker"]
+  Store.all.each do |store|
+    if store.name == budget_params["checker"]
+      @checker = []
+      @checker.push(budget_params["checker"])
+    end
+  end
+  render :show
+end
+
+private
+def budget_params
+  params.permit(:checker)
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   end
 
   get "/budget", to: "budgets#show"
+  post "/budget/check", to: "budgets#check"
 end
